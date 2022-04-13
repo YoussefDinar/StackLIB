072efc2ffa9e7f7c7ca791cf430758e760f8009e
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-04-14 04:29:37 +1000

    Update Stack.h

--- Lib_Stack/Stack.h
+++ Lib_Stack/Stack.h
@@ -1,16 +1,71 @@
 #pragma once
+#include<iostream>
+#include "pch.h"
 #include"Node.h"
-class Stack
+
+using namespace std;
+
+template <class T>
+class Stack 
 {
 public:
 	Stack();
-	void push(Node* n);
+	void push(Node<T>* n);
 	void pop();
 	bool is_empty() const;
 	void print_stack() const;
-	Node* sommet() const;
+	Node<T>* sommet() const;
 	~Stack();
 private:
-	Node* Top;
+	Node<T>* Top;
 };
 
+template <class T>
+Stack<T>::Stack()
+{
+	this->Top = nullptr;
+}
+
+template <class T>
+void Stack<T>::push(Node<T>* n)
+{
+	n->Next = this->Top;
+	this->Top = n;
+}
+template <class T>
+void Stack<T>::pop()
+{
+	Node<T>* tmp = this->Top;
+
+	this->Top = this->Top->Next;
+	delete tmp;
+	tmp = nullptr;
+}
+template <class T>
+bool Stack<T>::is_empty() const
+{
+	return (this->Top == nullptr);
+}
+template <class T>
+void Stack<T>::print_stack() const
+{
+	Node<T>* tmp = this->Top;
+	while (tmp != nullptr) {
+		tmp->afficher();
+		tmp = tmp->Next;
+	}
+}
+template <class T>
+Node<T>* Stack<T>::sommet() const
+{
+	return this->Top;
+}
+template <class T>
+Stack<T>::~Stack()
+{
+
+	while (this->is_empty() != true) {
+		this->pop();
+	}
+	std::cout << "destructeur de la classe stack" << std::endl;
+}
