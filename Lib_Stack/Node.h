6292600f52af7e37c440c442657c3fd01257b12c
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-04-14 04:28:53 +1000

    Update Node.h

--- Lib_Stack/Node.h
+++ Lib_Stack/Node.h
@@ -1,16 +1,44 @@
 #pragma once
-typedef int T;
+#include "pch.h"
+#include <iostream>
+using namespace std;
+template <class T>
 class Node
 {
 public:
 	Node(T data);
 	void afficher() const;
 	~Node();
-	Node* get_Next() const; // getter en lecture
-	void set_Next(Node*); // setter en ecriture
+	void set_Next(Node<T>* );
+	template <class T>
 	friend class Stack;
 private:
 	T Data;
-	Node* Next;
+	Node<T> *Next;
 
 };
+
+template <class T>
+Node<T>::Node(T data)
+{
+	this->Data = data;
+	this->Next = nullptr;
+}
+template <class T>
+void Node<T>::afficher() const
+{
+	std::cout << this->Data << std::endl;
+}
+
+
+template <class T>
+void Node<T>::set_Next(Node<T>* n)
+{
+	this->Next = n;
+}
+
+template <class T>
+Node<T>::~Node()
+{
+	std::cout << "destructeur de la classe Node" << std::endl;
+}
